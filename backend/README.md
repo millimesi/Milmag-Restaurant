be3d42ea9c147e97ce5cd4ef876a4d20e0284ee1
Magret <oyedeleabiodun79@gmail.com>
2024-09-11 13:03:39 -0230

    Files moved to backend directory

---
+++ backend/README.md
@@ -0,0 +1,12 @@
+Still to be modified...
+
+(DATABASE)
+What to do to get the database running:
+1. Download postgresql on your terminal (Get this from https://www.postgresql.org/)
+2. On terminal, follow this video to create a 'user' and 'database' on postgresql terminal https://www.youtube.com/watch?v=vxu1RrR0vbw 
+3. Install node v20.17.0   (Latest LTS: Iron)
+4. run 'npm install' to install the dependencies in package.json on vscode.
+5. run 'npm start', you should see a message 'Database & tables synced'
+6. Optional: You can also download 'PGAdmin4' and connect the database to it
+7. Don't forget to create .gitignore file to add 'node_modules/' and '.env'
+8. The .env_examples file contains the environment variable details that is needed in the application.
---
+++ backend/utils/dbConfig.js
@@ -0,0 +1,21 @@
+// Configures the db
+
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
+  host: process.env.PGHOST,
+  dialect: process.env.DIALECT,
+  logging: false, // Set to true for SQL query logging
+
+  pool: {
+    max: parseInt(process.env.POOLMAX, 10), // Maximum number of connections in the pool
+    min: parseInt(process.env.POOLMIN, 10), //  Minimum number of connections in the pool
+    acquire: parseInt(process.env.POOLACQUIRE, 10), // Maximum time (in ms) to acquire a connection before throwing an error
+    idle: parseInt(process.env.POOLIDLE, 10), // Maximum time (in ms) a connection can be idle before being released
+  },
+});
+
+export default sequelize;
