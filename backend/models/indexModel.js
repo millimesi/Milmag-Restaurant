38b8a18d4dd5acbef9701795642c204c9a991084
Magret <oyedeleabiodun79@gmail.com>
2024-09-12 10:51:50 -0230

    Configured db with the user model

--- backend/models/indexModel.js
+++ backend/models/indexModel.js
@@ -1,12 +1,12 @@
 // File to handle model initialization and synchronization:
 
 import sequelize from '../utils/dbConfig.js'; // Import the sequelize instance
-// import User from './userModel.js'; // Import the User model
+import User from './userModel.js'; // Import the User model
 
 const db = {};
 
 // Initialize the models
-// db.User = User;
+db.User = User;
 
 // Synchronize models
 const syncDb = async () => {
