32d0f8e58b9f8c834f8631ddf2a0f6644aab6085
Magret <oyedeleabiodun79@gmail.com>
2024-10-02 13:42:10 -0230

    Setup frontend logic for the menu page

---
+++ frontend/milmag-frontend/src/components/salad.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Salad = () => {
+  return (
+    <div>
+        <h1>Salad</h1>
+        <ul>
+            <li>Cheese Salad</li>
+            <li>Bacon Salad</li>
+            <li>Veggie Salad</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Salad;
