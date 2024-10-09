e39f26dfe10c2e04a54ab08f0d6c88b56e9f361b
Magret <oyedeleabiodun79@gmail.com>
2024-10-09 10:13:47 -0230

    Password Reset Feature/Setup password reset feature on the backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express';
 const router = express.Router();
 
-import { register, login } from '../controllers/userController.js';
+import { register, login, allUsers, AUser, forgotPassword, resetPassword } from '../controllers/userController.js';
 import { authenticate } from '../middlewares/authenticate.js';
 
 router.post('/register', register); // Routes to register. POST http://localhost:8000/api/v1/users/register
 router.post('/login', login); // Routes to login. POST http://localhost:8000/api/v1/users/login
+router.get('/', allUsers); // Routes to get all users
+router.get('/:email', AUser);
+router.put('/forgotPassword', forgotPassword); // Routes to forgot Password. PUT http://localhost:8000/api/v1/users/forgotPassword Supply email in req.body
+router.put('/resetPassword', resetPassword); // Routes to reset Password. You will be directed from email address. PUT http://localhost:8000/api/v1/users/resetPassword Supply newPassword, resetLink in req.body
+
 
 export default router;
