276e0d651e67970d0cdba9b8ba7c4321f1c26b4e
millimesi <millionmesi1@gmail.com>
2024-10-24 16:15:54 +0300

    Modify  Our resturant Service Section
    Make the Description to contain of three elements

--- frontend/src/pages/RestaurantDescription.jsx
+++ frontend/src/pages/RestaurantDescription.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import "./RestaurantDescription.css";
 import Dining from "../assets/dining.png";
 import Service from "../assets/waiters.png";
+import Food from "../assets/food.png";
 
 const RestaurantDescription = () => {
   return (
-    <div className="restaurant-desc">
+    <div className="service-desc">
       <h1>Our Restaurant</h1>
       <p>for your comfort, service and freshness</p>
-      <div className="main-container">
-        <div className="dining-hall">
+      <div className="service-desc-cont">
+        <div className="desc-left">
           <p>
             Step into our beautifully designed restaurant, where contemporary
             elegance meets cozy charm. Soft lighting and tasteful decor set the
@@ -19,13 +20,23 @@ const RestaurantDescription = () => {
         </div>
         <hr />
 
-        <div className="dining-hall">
-          <img src={Service} alt="Milmag Service" />
+        <div className="desc-right">
+          <img src={Service} alt="Milmag Waiters" />
           <p>
             Step into our beautifully designed restaurant, where contemporary
             elegance meets cozy charm. Soft lighting and tasteful decor set the
             perfect backdrop for a memorable dining experience.
           </p>
+        </div>
+        <hr />
+
+        <div className="desc-left">
+          <p>
+            Step into our beautifully designed restaurant, where contemporary
+            elegance meets cozy charm. Soft lighting and tasteful decor set the
+            perfect backdrop for a memorable dining experience.
+          </p>
+          <img src={Food} alt="Fresh food" />
         </div>
         <hr />
       </div>
