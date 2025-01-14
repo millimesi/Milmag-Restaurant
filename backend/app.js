0f2a1c42a9e24f38c4ff0dd2d15dfd74bc8e388c
Magret <belloabiodun17@gmail.com>
2025-01-14 15:48:20 -0330

    Implemented stripe payment system

--- backend/app.js
+++ backend/app.js
@@ -7,16 +7,25 @@ import db from "./models/indexModel.js";
 import userRoutes from "./routes/userRoutes.js";
 import menuRoutes from "./routes/menuRoutes.js";
 import reservationRoutes from "./routes/reservationRoutes.js";
+import paymentRoutes from "./routes/paymentRoutes.js";
+
 
 export const app = express();
 const port = process.env.PORT || 8000;
 
+const stripe = process.env.REACT_STRIPE_SECRET_KEY;
+
+
 // CORS middleware
 app.use(
   cors({
-    origin: `http://localhost:${process.env.FRONTENDPORT}`, // Allow requests from this origin
-    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods
+    origin: [
+      `http://localhost:${process.env.FRONTENDPORT}`,
+      `http://localhost:${process.env.PORT}`
+    ], // Allow requests from these origins
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    // allowedHeaders: ["Content-Type", "Authorization", "cookies"],
   })
 );
 
@@ -31,10 +40,14 @@ app.get("/", (req, res) => {
   res.send("Milmag Restaurant Application");
 });
 
+// Static middleware
+// app.use("/images", express.static("images"));
+
 // Routes Middlewares
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/menu", menuRoutes);
 app.use("/api/v1/reservation", reservationRoutes);
+app.use("/api/v1/payments", paymentRoutes);
 
 export const startServer = async () => {
   try {
